f8102829475dbe5b1030f336925bd74b81d1f776
Florence <theindependentfloppy@gmail.com>
2020-10-04 14:17:06 +0200

    Intro menu

---
+++ Assets/Scripts/IntroClickBunny.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class IntroClickBunny : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject Menu;
+    public GameObject IntroText;
+
+    private Animator anim;
+    private AudioSource audioSource;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        anim.SetTrigger("IsTouched");
+        StartCoroutine(StartTextIntro());
+    }
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private IEnumerator StartTextIntro()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Menu.SetActive(false);
+        IntroText.SetActive(true);
+    }
+
+    public void PlaySound()
+    {
+        audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/IntroSound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroSound : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound()
+    {
+        audioSource.Play();
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+}
